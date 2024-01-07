8e12af4545d89368b6a23e246c8f177cbff63b64
jobel v. golde <jobelgolde43@gmail.com>
2024-01-07 08:09:52 +0800

    modify index.html, index.php

---
+++ webPages/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'BMAIS';
+
+$dns = "mysql:host=$host;dbname=$dbname";
+
+$pdoObject = new PDO($dns, $user, $pass);
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['form_id']) && $_POST['form_id'] === 'form_1') {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+       
+    } elseif (isset($_POST['form_id']) && $_POST['form_id'] === 'form_2') {
+       
+    }
+   
+    header("Location: dashboard.php");
+    exit();
+}
+
